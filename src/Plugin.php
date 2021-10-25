fc0e9d0394ec3a908a4f187720567ecfbca09100
Kevin Porras <kporras07@gmail.com>
2021-10-25 17:54:43 -0600

    Validate and sort workflows.

--- src/Plugin.php
+++ src/Plugin.php
@@ -46,18 +46,46 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         $repositoryManager = $this->composer->getRepositoryManager();
         $localRepository = $repositoryManager->getLocalRepository();
         $packages = $localRepository->getPackages();
-        $packages_info = [];
+        // Quicksilver workflows as per https://pantheon.io/docs/quicksilver#hooks.
+        $wf_info = [];
 
         foreach ($packages as $package) {
             if ($package->getType() !== 'quicksilver-script') {
                 continue;
             }
+            $package_name = $package->getName();
             $extra = $package->getExtra();
             if (!empty($extra['pantheon-quicksilver'])) {
-                $packages_info[$package->getName()] = $extra['pantheon-quicksilver'];
+                $workflows = reset($extra['pantheon-quicksilver']);
+                foreach ($workflows as $workflow) {
+                    if (!$this->isValidWorkflow($workflow)) {
+                        // @todo Does this work?
+                        $event->getIO()->warning('Invalid workflow info for package !package', [
+                            '!package' => $package_name,
+                        ]);
+                    }
+                    if (!isset($wf_info[$workflow['wf_type']])) {
+                        // Create index if it does not exist.
+                        $wf_info[$workflow['wf_type']] = [];
+                    }
+                    // This assumes there is only one $wf_type per package. @todo confirm.
+                    $wf_info[$workflow['wf_type']][$package_name] = $workflow;
+                }
             }
         }
-        var_dump($packages_info);
+        // Sort each wf_type.
+        // @todo validate sorting works as expected.
+        foreach ($wf_info as &$wf_type) {
+            usort($wf_type, function ($wf_a, $wf_b) {
+                $weight_a = !empty($wf_a['weight']) ? $wf_a['weight'] : 0;
+                $weight_b = !empty($wf_b['weight']) ? $wf_b['weight'] : 0;
+                if ($weight_a === $weight_b) {
+                    return 0;
+                }
+                return ($weight_a > $weight_b) ? 1 : -1;
+            }); 
+        }
+        var_dump($wf_info);
 
         /*$yaml_data = [
             // 'content-hash' => $content_hash,
@@ -66,6 +94,19 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 
         $yaml = Yaml::dump($yaml_data);*/
 //        file_put_contents('composer-manifest.yaml', $yaml);
+    }
+
+    /**
+     * Validate that workflow structure complies with pantheon-yml-editor. 
+     */
+    protected function isValidWorkflow(array $workflow)
+    {
+        // Weight is being treated as optional.
+        if (!isset($workflow['wf_type']) || !isset($workflow['stage'])) {
+            return false;
+        }
+        // @todo More validations could be added (e.g. stage vs wf_type).
+        return true;
     }
 
     /**
