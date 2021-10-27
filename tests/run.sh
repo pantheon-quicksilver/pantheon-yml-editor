be65679ede333658a6404c4340c745f8eac5068a
Kevin Porras <kporras07@gmail.com>
2021-10-27 16:46:16 -0600

    Add basic testing.

--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo 'No tests'
+set -e
+
+# Prepare drupal-project.
+cd drupal-project
+composer install
+composer config repositories.ymleditor path pantheon-yml-editor
+composer require pantheon-quicksilver/pantheon-yml-editor:"*"
+
+# Require wakeup script.
+composer require pantheon-quicksilver/wakeup
+diff -q pantheon.yml pantheon-yml-editor/tests/fixtures/1-pantheon.yml
+
+composer remove pantheon-quicksilver/wakeup
+diff -q pantheon.yml pantheon-yml-editor/tests/fixtures/2-pantheon.yml
