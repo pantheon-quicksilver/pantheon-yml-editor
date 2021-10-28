c6315956b78e4b737ef1231ad8811fb937431ffa
Kevin Porras <kporras07@gmail.com>
2021-10-28 14:34:49 -0600

    Add some debugging.

--- src/Plugin.php
+++ src/Plugin.php
@@ -82,6 +82,8 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     public function updatePantheonYml(Event $event)
     {
         var_dump($_SERVER);
+        var_dump(getenv('PANTHEON_RUNTIME'));
+        var_dump($_ENV['PANTHEON_RUNTIME']);
         // This should not run in Pantheon Integrated composer.
         if (getenv('PANTHEON_ENVIRONMENT')) {
             return;
